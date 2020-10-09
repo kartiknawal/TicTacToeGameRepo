39af909159740823846ce79c28ec6605baa987ca
kartiknawal <kartiknawal.7@gmail.com>
2020-10-09 08:29:35 +0530

    [kartiknawal] Add. functionality to create a board

---
+++ TicTacToeGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGameNew
+{
+    class TicTacToeGame
+    {
+        char[] board = new char[10];
+        public void CreateBoard()
+        {
+            int i;
+            for (i = 1; i < 10; i++)
+            {
+                board[i] = ' ';
+            }
+        }
+    }
+}
